279c816212cee787b3f788122fb34152e3fe64d1
AcNEO <37327658+AcNEO@users.noreply.github.com>
2018-03-13 16:54:31 +0800

    Create Main.php

---
+++ AcNEO/SayHello/Main.php
@@ -0,0 +1,21 @@
+<?php
+namespace AcNEO\SayHello;
+
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+
+class Main extends PluginBase implements Listener {
+
+public function onLoad() {
+$this->getServer()->getLogger()->notice("This Is A Very Nano-Plugin That Do A Simple Hello Player");
+}
+
+public function onJoin(PlayerJoinEvent $ev) {
+$p = $ev->getPlayer();
+$p_name = $p->getName();
+$p->sendMessage("Hello ". $p_name);
+}
+
+}
+?>
